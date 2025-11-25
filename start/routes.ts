497123cc0ec4fe50f4a4a0bee6146c325960c7fc
mbp16-02 <syarif.hidayat@biofarma.co.id>
2025-11-25 22:01:47 +0700

    add swagger & clear data generate

--- start/routes.ts
+++ start/routes.ts
@@ -8,33 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
-
-const UserController = () => import('#controllers/user_controller')
-
-const RoleController = () => import('#controllers/role_controller')
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
 
 router.get('/', async () => {
   return {
     hello: 'Backend API',
   }
 })
-// CRUD User
-router
-  .group(() => {
-    router.get('/', [UserController, 'index'])
-    router.get('/:id', [UserController, 'show'])
-    router.post('/', [UserController, 'store'])
-    router.put('/:id', [UserController, 'update'])
-    router.delete('/:id', [UserController, 'destroy'])
-  })
-  .prefix('users')
-// CRUD Role
-router
-  .group(() => {
-    router.get('/', [RoleController, 'index'])
-    router.get('/:id', [RoleController, 'show'])
-    router.post('/', [RoleController, 'store'])
-    router.put('/:id', [RoleController, 'update'])
-    router.delete('/:id', [RoleController, 'destroy'])
-  })
-  .prefix('roles')
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
+})
